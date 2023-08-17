b751ea14d27af47d3066e0c4b5b8506fae7b4088
Aksshai Nair <aksshai07nair@gmail.com>
2023-08-17 14:11:09 +0530

    Code Upload

---
+++ main.py
@@ -0,0 +1,117 @@
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+import ml
+
+GRID_SIZE = 11
+CELL_SIZE = 60
+GRID = ml.get_grid()
+path = []
+invalid_start_flag = False
+
+def get_final_coordinates():
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            if GRID[row][col] == 2:
+                return(row,col)
+    return (0,0)
+
+final_coordinates = get_final_coordinates()
+
+def get_colors(row, col):
+    color, line = " ", " "
+    if (GRID[row][col]) == 0:   #path
+        color = "white"
+        line = "black"
+    elif (GRID[row][col]) == 2: #goal
+        color = "green"  
+        line = "white"
+    elif (GRID[row][col]) == 3: #current walking path
+        color = "red"
+        line = "white"
+    else:                       #wall
+        color = "black"
+        line = "white"
+
+    return line, color
+
+# def on_cell_click(pos):
+#     row = pos[1] // CELL_SIZE
+#     col = pos[0] // CELL_SIZE
+#     print(f"Cell clicked at row {row+1}, column {col+1}")
+
+
+def input_validator(text):
+    try:
+        value = int(text)
+        if 1 <= value <= GRID_SIZE:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+    
+def reset_grid():
+    global path, GRID
+    path = []
+    GRID = [[0 if cell == 3 else cell for cell in row] for row in GRID]
+    final_row, final_col = final_coordinates
+    GRID[final_row][final_col] = 2
+
+def input_handler(start_coordinates):
+    reset_grid()
+    global path, invalid_start_flag
+    start_row, start_col = start_coordinates.split(",")
+    start_row = int(start_row)
+    start_col = int(start_col)
+
+    if input_validator(start_row) and input_validator(start_col):
+        if GRID[start_row-1][start_col-1]!=1:
+            invalid_start_flag = False
+            path  = ml.get_path(start_row-1, start_col-1)
+            del path[-1]
+        else:
+            invalid_start_flag = True
+    else:
+        print("Invalid input. Please enter numbers between 1 and 11.")
+
+def update_grid():
+
+    if not path:
+        return
+    else:
+        row, col = path[0]
+        GRID[row][col] = 3  # Set cell color to red
+        del path[0]
+
+def draw_handler(canvas):
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            x0 = col * CELL_SIZE
+            y0 = row * CELL_SIZE
+            x1 = x0 + CELL_SIZE
+            y1 = y0 + CELL_SIZE
+
+            line, color = get_colors(row, col)
+
+            if color=="black":
+                canvas.draw_image(wall, (wall_w/2, wall_h/2), (wall_w, wall_h), (x0+20, y0+20), (60, 60))
+            else:
+                canvas.draw_polygon([(x0, y0), (x1, y0), (x1, y1), (x0, y1)], 1, line, color)
+                canvas.draw_text(f"{(row+1)} {(col+1)}", (x0+20,y0+30), 15, "black")
+
+ml.train()
+
+timer_interval = 200
+timer = simplegui.create_timer(timer_interval, update_grid)
+
+frame = simplegui.create_frame("Maze Runner", GRID_SIZE * CELL_SIZE, GRID_SIZE * CELL_SIZE)
+frame.add_input("Starting (a,b) : ", input_handler, 100)
+wall = simplegui.load_image('/Users/aksshainair/Desktop/aksshaipy/SEM4IndividualProject/wall.jpeg')
+# goal = simplegui.load_image('/Users/aksshainair/Desktop/aksshaipy/SEM4IndividualProject/goal.png')
+wall_w, wall_h = wall.get_width(), wall.get_height()
+# goal_w, goal_h = goal.get_width(), goal.get_height()
+frame.set_draw_handler(draw_handler)
+
+# frame.set_mouseclick_handler(on_cell_click)
+
+timer.start()
+frame.start()
